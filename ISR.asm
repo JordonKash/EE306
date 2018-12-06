ee0c55ea4d7efd92cdffc16733032d0ca1ba84d5
JordonKash <45576748+JordonKash@users.noreply.github.com>
2018-12-05 19:01:51 -0600

    Add files via upload

---
+++ ISR.asm
@@ -0,0 +1,54 @@
+; ISR.asm
+; Name:
+; UTEid: 
+; Keyboard ISR runs when a key is struck
+; Checks for a valid RNA symbol and places it at x4600
+               .ORIG x2600
+
+;Read KBDR(xFE02) to get character typed
+
+START 	LDI R1, polling
+	BRzp START
+ 
+
+LDI R3, CharTyped
+
+LD R4, CharA
+ADD R5, R4, R3
+BRz Vaild
+
+
+LD R4, CharC
+ADD R5, R4, R3
+BRz Vaild
+
+
+LD R4, CharU
+ADD R5, R4, R3
+BRz Vaild
+
+
+LD R4, CharG
+ADD R5, R4, R3
+BRz Vaild
+
+AND R3, R3, #0
+
+Vaild
+STI R3, Store
+
+;Check for A,C,U, or G AND update x4600
+
+;no loops
+
+RTI
+
+polling		.FILL xFE00
+CharTyped	.FILL xFE02
+CharA		.FILL x-41
+CharC		.FILL x-43
+CharU		.FILL x-55
+CharG		.FILL x-47
+Store		.FILL x4600
+
+		.END
---
+++ Main.asm
@@ -0,0 +1,106 @@
+; Main.asm
+; Name:
+; UTEid: 
+; Continuously reads from x4600 making sure its not reading duplicate
+; symbols. Processes the symbol based on the program description
+; of mRNA processing.
+               .ORIG x4000
+; initialize the stack pointer
+; First, write the 3 steps of setup
+
+; set up the keyboard interrupt vector table entry
+
+; enable keyboard interrupts
+
+; start of actual program
+
+LEA R1, S1
+LDI R2, ISRstart
+state_machine_loop
+	ADD R0, R1, #0
+	TRAP x22
+	ADD R1, R1, R2
+	LDR R1, R1, #0
+	BR state_machine_loop
+
+
+;START	
+;	.FILL NEXT_STATEA
+;	.FILL NEXT_STATEC
+;	.FILL NEXT_STATEG
+;	.FILL NEXT_STATEU
+;       .FILL OUTPUT
+
+S1	.FILL S2
+	.FILL S1
+	.FILL S1
+	.FILL S1
+	.STRINGZ "HEY" ;OUTPUT CHARACTER
+
+S2	.FILL S1
+	.FILL S1
+	.FILL S1
+	.FILL S3
+	.STRINGZ "A"
+
+S3	.FILL S1
+	.FILL S1
+	.FILL S4
+	.FILL S1
+	.STRINGZ "U"
+	
+S4	.FILL S5     ;;START
+	.FILL S5
+	.FILL S5
+	.FILL S6
+	.STRINGZ "G|"
+
+S5	.FILL S5
+	.FILL S5
+	.FILL S5
+	.FILL S6
+	.STRINGZ "HEY" ;OUTPUT CHARACTER
+
+S6	.FILL S7
+	.FILL S5
+	.FILL S9
+	.FILL S6
+	.STRINGZ "U"
+
+S7	.FILL S8
+	.FILL S5
+	.FILL S8
+	.FILL S6
+	.STRINGZ "HEY" ;OUTPUT CHARCTER
+
+S8	TRAP x25
+
+S9	.FILL S7
+	.FILL S5
+	.FILL S5
+	.FILL S6
+	.STRINGZ "G"
+	
+
+;START	
+;	.FILL NEXT_STATEA
+;	.FILL NEXT_STATEC
+;	.FILL NEXT_STATEG
+;	.FILL NEXT_STATEU
+;       .FILL OUTPUT
+
+
+
+
+
+
+
+
+
+StackStart 	.FILL x4000
+ISRstart	.FILL x2600
+Vector 		.FILL x0180
+BitsKBSR	.FILL xFE00
+bitchange	.FILL x4000
+
+		.END
